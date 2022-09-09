0d3f92c64546ae31f0cef67837b1610a1befff2b
KD Ninama <krunal16062000@gmail.com>
2022-09-09 14:03:30 +0530

    Router Demo

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {  Link} from "react-router-dom";
+function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/About">About</Link></li>
+        <li><Link to="/Contect">Contect</Link></li>
+        <li><Link to="/User/kd">kd</Link></li>
+        <li><Link to="/User/Rahul">Rahul</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
