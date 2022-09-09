f5a7b37574b723c1dd29c65126556efb60ded013
KD Ninama <krunal16062000@gmail.com>
2022-09-09 14:02:43 +0530

    Router Demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Contect from "./Components/Contect";
+import About from "./Components/About";
+import NavBar from "./Components/NavBar";
+import User from "./Components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Routing</h1>
+   
+      <BrowserRouter>
+      <NavBar/>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/Contect" element={<Contect />} />
+          <Route path="/User/ :id" element={<User />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
